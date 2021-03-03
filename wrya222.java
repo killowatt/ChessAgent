6455def25653505356fffe344da8a49d6d7d2748
killowatt <killowatt2213@gmail.com>
2021-03-03 00:18:24 -0500

    Initial commit

---
+++ wrya222.java
@@ -0,0 +1,133 @@
+import edu.uky.ai.chess.Agent;
+import edu.uky.ai.chess.state.*;
+
+class epic
+{
+    int value;
+    State state;
+
+    public epic(int value, State state)
+    {
+        this.value = value;
+        this.state = state;
+    }
+
+    static epic max(epic left, epic right)
+    {
+        if (left.value >= right.value)
+            return left;
+        else
+            return right;
+    }
+
+    static epic min(epic left, epic right)
+    {
+        if (left.value <= right.value)
+            return left;
+        else
+            return right;
+    }
+}
+
+/**
+ * @author William Yates
+ */
+public class wrya222 extends Agent 
+{
+    public wrya222() 
+    {
+        super("wrya222");
+    }
+
+    @Override
+    protected State chooseMove(State current) 
+    {
+        epic p = minmax(1, current, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        State s = p.state;
+
+        if (s == null) {
+            System.out.println("Null state");
+            return null;
+        }
+
+        for (State ss : current.next())
+        {
+            System.out.println("<" + ss.toString() + " ?= " + s.toString() + ">");
+            if (ss.toString().equals(s.toString()))
+                return ss;
+        }
+
+        System.out.println("BUG, no matching states.");
+        return null; // lol
+    }
+
+    private static epic minmax(int depth, State state, int alpha, int beta)
+    {
+        if (state.check && state.player == Player.WHITE)
+            return new epic(10000, state);
+
+        if (state.check && state.player == Player.BLACK)
+            return new epic(-10000, state);
+
+        if (state.over || state.movesUntilDraw == 0)
+            return new epic(0, state);
+
+        if (depth == 0)
+            return new epic(getBoardValue(state.board, state.player), state);
+
+        if (state.player == Player.WHITE)
+        {
+            epic value = new epic(Integer.MIN_VALUE, null);
+
+            for (State next : state.next())
+            {
+                value = epic.max(value, minmax(depth - 1, next, alpha, beta));
+                alpha = Math.max(alpha, value.value);
+                if (alpha >= beta)
+                    break;
+            }
+            return value;
+        }
+        else
+        {
+            epic value = new epic(Integer.MAX_VALUE, null);
+            for (State next : state.next())
+            {
+                value = epic.min(value, minmax(depth - 1, next, alpha, beta));
+                beta = Math.min(beta, value.value);
+                if (beta <= alpha)
+                    break;
+            }
+            return value;
+        }
+    }
+
+    private static int getBoardValue(Board board, Player player)
+    {
+        int score = 0;
+        for (Piece piece : board)
+        {
+            int value = getPieceValue(piece);
+            score += piece.player == player ? value : -value;
+        }
+        return score;
+    }
+
+    private static int getPieceValue(Piece piece)
+    {
+        if (piece instanceof Pawn)
+            return 1;
+        else if (piece instanceof Knight)
+            return 3;
+        else if (piece instanceof Bishop)
+            return 3;
+        else if (piece instanceof Rook)
+            return 5;
+        else if (piece instanceof Queen)
+            return 9;
+        else if (piece instanceof King)
+            return 100;
+        else
+            return 0;
+    }
+}
